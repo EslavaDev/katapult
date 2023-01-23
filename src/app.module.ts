d0844047f6b1fdc7603b4a72fbe2bfb43c9c70bb
eslavadev <danieleslava52@gmail.com>
2023-01-22 22:03:51 -0500

    add in app module account and supplier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { BanksModule } from './modules/banks/banks.module';
+import { BanksService } from './modules/banks/banks.service';
+import { SupplierService } from './modules/supplier/supplier.service';
+import { AccountsService } from './modules/accounts/accounts.service';
+import { AccountsModule } from './modules/accounts/accounts.module';
+import { SupplierModule } from './modules/supplier/supplier.module';
 
 @Module({
-  imports: [DatabaseModule, BanksModule],
+  imports: [
+    DatabaseModule,
+    BanksModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AccountsModule,
+    SupplierModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BanksService, SupplierService, AccountsService],
 })
 export class AppModule {}
