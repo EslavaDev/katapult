2ef28788f56fef7bbdeae2d794f6c2773ce00f29
eslavadev <danieleslava52@gmail.com>
2023-01-22 22:01:23 -0500

    fix sequalize

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -1,13 +1,16 @@
 import { Sequelize } from 'sequelize-typescript';
+import { Account } from 'src/modules/accounts/accounts.entity';
 import { Bank } from 'src/modules/banks/bank.entity';
+import { Supplier } from 'src/modules/supplier/supplier.entity';
+import { SEQUELIZE } from '../constants';
 import { databaseConfig } from './database.config';
 
 export const databaseProviders = [
   {
-    provide: 'SEQUELIZE',
+    provide: SEQUELIZE,
     useFactory: async () => {
       const sequelize = new Sequelize(databaseConfig);
-      sequelize.addModels([Bank]);
+      sequelize.addModels([Bank, Account, Supplier]);
       await sequelize.sync();
       return sequelize;
     },
