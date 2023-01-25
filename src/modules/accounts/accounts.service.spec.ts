622f4a6cdaa27fb3037ca7b5f7b09271bc238827
eslavadev <danieleslava52@gmail.com>
2023-01-24 22:41:22 -0500

    fix test account error

--- src/modules/accounts/accounts.service.spec.ts
+++ src/modules/accounts/accounts.service.spec.ts
@@ -1,3 +1,4 @@
+import { ACCOUNTS_REPOSITORY } from '../../core/constants/index';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountsService } from './accounts.service';
 
@@ -6,7 +7,18 @@ describe('AccountsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountsService],
+      providers: [
+        {
+          provide: ACCOUNTS_REPOSITORY,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+        AccountsService,
+      ],
     }).compile();
 
     service = module.get<AccountsService>(AccountsService);
